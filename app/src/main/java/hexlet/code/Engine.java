8b24f1bf0c860ed6345c8b6c5525884e002def0a
Avicte <125468647+Avicte@users.noreply.github.com>
2023-03-11 22:04:27 +0300

    Update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,9 +1,10 @@
 package hexlet.code;
 
 public class Engine {
+    static final int maxCount = 3;
 
     public static void endingString(int count, String userName) {
-        if (count == 3) {
+        if (count == maxCount) {
             System.out.println("Congratulations, " + userName + "!");
         } else {
             System.out.println("Game Over, " + userName + "!");
